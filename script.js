a021d6a5c5003c851723ff85aebdf461b9624a81
Farah Saleem <fs8578058@gmail.com>
2025-10-23 00:57:24 -0700

    add function for simply and clean code

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ let silder = document.querySelector(".slider");
 let images = document.querySelectorAll(".image");
 
 
-
+let length = images.length
 let sliderNum = 1;
+
+const nextSlide = () => {
+        silder.style.transform = `translateX(-${sliderNum * 800}px)`;
+        sliderNum++;
+}
+
+const secSlide = () => {
+        silder.style.transform = `translateX(0px)`;
+        sliderNum = 1;
+}
+
 right.addEventListener("click", () => {
-        if (sliderNum < images.length) {
-                silder.style.transform = `translateX(-${sliderNum * 800}px)`;
-                sliderNum++;
-        } else {
-                silder.style.transform = `translateX(0px)`;
-                sliderNum = 1;
-        }
+        sliderNum < length ?  nextSlide() : secSlide();
 })
 
