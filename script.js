a3ed9eb91816a47929c6a9122158076f0c29c3fd
Farah Saleem <fs8578058@gmail.com>
2025-10-31 01:11:43 -0700

    dots section complete

--- script.js
+++ script.js
@@ -18,23 +18,64 @@ const secSlide = () => {
 }
 
 right.addEventListener("click", () => {
-        sliderNum < length ?  nextSlide() : secSlide();
+        sliderNum < length ? nextSlide() : secSlide();
+        changeColor();
 })
 
 
 const prevSlide = () => {
-        silder.style.transform = `translateX(-${(sliderNum-2)*800}px)`;
+        silder.style.transform = `translateX(-${(sliderNum - 2) * 800}px)`;
         sliderNum--;
 }
 
 const lastSlide = () => {
-        silder.style.transform = `translateX(-${(length-1)*800}px)`;
+        silder.style.transform = `translateX(-${(length - 1) * 800}px)`;
         sliderNum = length;
 }
 
 left.addEventListener("click", () => {
-        sliderNum > 1 ?  prevSlide() : lastSlide();
+        sliderNum > 1 ? prevSlide() : lastSlide();
+        changeColor();
 })
 
 
 
+//start for dots
+
+
+const buttom = document.querySelector(".buttom");
+
+for (let i = 0; i < length; i++) {
+        const div = document.createElement("div");
+        div.className = "button";
+        buttom.appendChild(div);
+}
+
+
+const buttons = document.querySelectorAll(".button");
+
+buttons[0].style.backgroundColor = "white";
+
+const resetbg = () => {
+        buttons.forEach((button) => {
+                button.style.backgroundColor = "transparent";
+        })
+}
+
+buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+          silder.style.transform = `translateX(-${i*800}px)`;
+          resetbg();
+          button.style.backgroundColor = "white";
+
+        })
+})
+
+
+
+const changeColor = () => {
+        resetbg();
+        buttons[sliderNum-1].style.backgroundColor = "white";
+}
+
+//end dots
